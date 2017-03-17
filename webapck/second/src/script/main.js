6d3629856d89bb615a1a30c6fdb64fe9fed903b2
Mr. Super <linzechao44@163.com>
2017-03-17 23:16:27 +0800

    添加配置文件

--- webapck/second/src/script/main.js
+++ webapck/second/src/script/main.js
@@ -1,1 +1,28 @@
+'use strict';
+
 console.log('This is main.js');
+
+const abc = 123;
+
+console.log(abc);
+
+// (() => console.log('Hi'))();
+
+
+
+const cccaaaa = 'Helo';
+let bb = 333;
+
+console.log(cccaaaa);
+
+for (let a = 0; a < 10; a++) {
+    //console.log(a);
+}
+
+{ 
+    let a = 1;
+    console.log(a);
+}
+
+
+
--- webapck/second/webpack.config.js
+++ webapck/second/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack'),
 
 // 多页面配置
 const ENTRY = {
-    index: './src/script/index.js',
-    login: './src/script/login.js',
+    // index: './src/script/index.js',
+    // login: './src/script/login.js',
     main: './src/script/main.js'
 }, PAGES = [];
 
@@ -14,7 +14,7 @@ for (let key in ENTRY) {
     PAGES.push(new htmlWebpackPlugin({
         filename: `${key}.html`,
         // filename: 'main-[hash].html',
-        template: 'main.html',
+        template: `./src/template/${key}.html`,
         inject: 'head',
         title: 'html webpack plugin title',
         date: new Date(),
@@ -23,16 +23,17 @@ for (let key in ENTRY) {
         // 排除指定模块
         // excludeChunks: ['login', 'index'],
         // 压缩
+        /*
         minify: {
             // 注释
             removeComments: true,
             // 空格
             collapseWhitespace: true
         }
+        */
     }));
 }
 
-console.log(PAGES);
 
 module.exports = {
     // 上下文
@@ -44,9 +45,9 @@ module.exports = {
     // 输出
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name]-[chunkhash].js',
+        filename: 'js/[name].js',
         // 上线前设置
-        publicPath: 'http://www.waqudao.com/'
+        // publicPath: 'http://www.waqudao.com/'
     },
 
     // 模块
@@ -68,6 +69,29 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loader: 'style-loader!css-loader!sass-loader'
+            },
+
+            // 解析ES6
+            // 耗时
+            // babel-loader、babel-core
+            // presets-latest = babel-preset-latest
+            // ① .babelrc文件
+            // ② package.json文件
+            // ③ 以下方式
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // 包含范围
+                include: [
+                    path.resolve(__dirname + 'src'),
+                    path.resolve(__dirname + 'test')
+                ],
+                // 忽略范围
+                exclude: path.resolve(__dirname + 'node_modules'),
+                // 解析该标准下的语法
+                query: {
+                    presets: ['es2015']
+                }
             }
         ]
     },
@@ -75,27 +99,29 @@ module.exports = {
     // 插件
     plugins: [
         new webpack.BannerPlugin('@author: MR.Super!'),
-        /*
-        new htmlWebpackPlugin({
-            filename: 'main.html',
-            // filename: 'main-[hash].html',
-            template: 'main.html',
-            inject: 'head',
-            title: 'html webpack plugin title',
-            date: new Date(),
-            // 指定引入那些模块
-            chunks: ['main'],
-            // 排除指定模块
-            excludeChunks: ['login', 'index'],
-            // 压缩
-            minify: {
-                // 注释
-                removeComments: true,
-                // 空格
-                collapseWhitespace: true
-            }
-        })
-        */
     ].concat(PAGES)
 };
+
+/*
+console.log(PAGES);
+new htmlWebpackPlugin({
+    filename: 'main.html',
+    // filename: 'main-[hash].html',
+    template: 'main.html',
+    inject: 'head',
+    title: 'html webpack plugin title',
+    date: new Date(),
+    // 指定引入那些模块
+    chunks: ['main'],
+    // 排除指定模块
+    excludeChunks: ['login', 'index'],
+    // 压缩
+    minify: {
+        // 注释
+        removeComments: true,
+        // 空格
+        collapseWhitespace: true
+    }
+})
+*/
 
